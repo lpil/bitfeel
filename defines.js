e9866be80bba630e54b554911706d70b5a26e9ad
Alexandre Bique <bique.alexandre@gmail.com>
2014-03-08 11:13:48 +0100

    Split the source code into many files

---
+++ defines.js
@@ -0,0 +1,72 @@
+const BT_TAP_TEMPO    = 3;
+const BT_METRONOME    = 9;
+
+const BT_MASTER       = 28;
+const BT_STOP         = 29;
+
+const BT_1_32T        = 43;
+const BT_1_32         = 42;
+const BT_1_16T        = 41;
+const BT_1_16         = 40;
+const BT_1_8T         = 39;
+const BT_1_8          = 38;
+const BT_1_4T         = 37;
+const BT_1_4          = 36;
+
+const BT_LEFT         = 44;
+const BT_RIGHT        = 45;
+const BT_TOP          = 46;
+const BT_BOTTOM       = 47;
+
+const BT_UNDO         = 119;
+const BT_DELETE       = 118;
+const BT_DOUBLE       = 117;
+const BT_QUANTIZE     = 116;
+
+const BT_FIXED_LENGTH = 90;
+const BT_AUTOMATE     = 89;
+const BT_DUPLICATE    = 88;
+const BT_NEW          = 87;
+const BT_RECORD       = 86;
+const BT_PLAY         = 85;
+
+const BT_SELECT       = 48;
+const BT_SHIFT        = 49;
+const BT_NOTE         = 50;
+const BT_SESSION      = 51;
+const BT_ADD_EFFECT   = 52;
+const BT_ADD_TRACK    = 53;
+const BT_OCTAVE_DOWN  = 54;
+const BT_OCTAVE_UP    = 55;
+const BT_REPEAT       = 56;
+const BT_ACCENT       = 57;
+const BT_SCALE        = 58;
+const BT_USER         = 59;
+const BT_MUTE         = 60;
+const BT_SOLO         = 61;
+const BT_SEND         = 62;
+const BT_RETURN       = 63;
+
+const BT_DEVICE       = 110;
+const BT_BROWSE       = 111;
+const BT_TRACK        = 112;
+const BT_CLIP         = 113;
+const BT_VOLUME       = 114;
+const BT_PAN_SEND     = 115;
+
+const ENC_0           = 71;
+const ENC_1           = 72;
+const ENC_2           = 73;
+const ENC_3           = 74;
+const ENC_4           = 75;
+const ENC_5           = 76;
+const ENC_6           = 77;
+const ENC_7           = 78;
+const ENC_VOL         = 79;
+
+const ENC_BPM         = 14;
+const ENC_SWING       = 15;
+
+const STATE_KEYBOARD  = "keyboard";
+const STATE_CLIP      = "clip";
+const STATE_DRUM      = "drum";
---
+++ push.js
@@ -0,0 +1,82 @@
+load("defines.js");
+
+function Push() {
+    var push = this;
+
+    this.state = STATE_KEYBOARD;
+    this.shift = false;
+
+    this.transport = host.createTransport();
+    this.trackBank = host.createTrackBankSection(8, 0, 0);
+    this.cursorTrack = host.createCursorTrackSection(0, 8);
+    this.masterTrack = host.createMasterTrackSection(0);
+
+    host.getMidiInPort(0).setMidiCallback(function(status, data1, data2) {
+        push.onMidi0(status, data1, data2);
+    });
+    host.getMidiInPort(0).setSysexCallback(function(data) {
+        push.onSysex0(data);
+    });
+    host.getMidiInPort(1).setMidiCallback(function(status, data1, data2) {
+        push.onMidi1(status, data1, data2);
+    });
+    host.getMidiInPort(1).setSysexCallback(function(data) {
+        push.onSysex1(data);
+    });
+
+    this.userControls = host.createUserControls(8);
+    for (var i = 0; i < 8; ++i) {
+        
+    }
+
+    println("Initialized");
+}
+
+Push.prototype.onMidi0 = function(status, data1, data2) {
+    println("unhandled midi 0: " + status + ", " + data1 + ", " + data2);
+}
+
+Push.prototype.onSysex0 = function(data) {
+    println("unhandled sysex 0: " + data);
+}
+
+Push.prototype.onMidi1 = function(status, data1, data2) {
+    if (status == 176) {
+        // button pushed
+        switch (data1) {
+        case BT_SHIFT:
+            this.shift = (data2 != 0);
+            return;
+
+        case BT_PLAY:
+            if (data2 == 0)
+                this.transport.togglePlay();
+            return;
+
+        case BT_RECORD:
+            if (data2 == 0)
+                this.transport.record();
+            return;
+
+        case BT_METRONOME:
+            if (data2 == 127)
+                this.transport.toggleClick();
+            return;
+
+        case ENC_SWING:
+            return;
+
+        case ENC_BPM:
+            println("shift: " + this.shift + ", data2: " + data2);
+            var amount = (this.shift ? 1 : 5) * (data2 == 1 ? 1 : -1);
+            this.transport.increaseTempo(amount, 1000);
+            return;
+        }
+    }
+
+    println("unhandled midi 1: " + status + ", " + data1 + ", " + data2);
+}
+
+Push.prototype.onSysex1 = function(data) {
+    println("unhandled sysex 1: " + data);
+}
